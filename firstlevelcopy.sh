281dd72cc4f5a338448ac34d568671ccb1b0ea1b
Ryan Stanley Falck <ryan.falck@hiphealth.ca>
2022-06-01 16:58:03 -0700

    All FSL related analyses and scripts

---
+++ firstlevelcopy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+chmod 777
+echo Set-Up
+sublist='001 009 023 030 033 035 063 078 095 101 106 109 113 127 138 141 151 153 159 170 188 190 205 209 223 276' #Subject List
+
+echo DO IT!
+for sub in $sublist
+do
+cp Now_Later_First_Level.fsf $sub
+echo $sub DONE!
+done
